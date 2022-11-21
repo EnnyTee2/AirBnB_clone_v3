2f3ccb46644c9363419a5741a64457687e2238b6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-21 21:29:37 +0100

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -88,22 +88,22 @@ class TestFileStorage(unittest.TestCase):
         """Test that save properly saves objects to file.json"""
 
     def test_get_db(self):
-    """ Tests method for obtaining an instance db storage"""
-    dic = {"name": "Cundinamarca"}
-    instance = State(**dic)
-    storage.new(instance)
-    storage.save()
-    get_instance = storage.get(State, instance.id)
-    self.assertEqual(get_instance, instance)
+        """ Tests method for obtaining an instance db storage """
+        dic = {"name": "Cundinamarca"}
+        instance = State(**dic)
+        storage.new(instance)
+        storage.save()
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
 
     def test_count(self):
-    """ Tests count method db storage """
-    dic = {"name": "Vecindad"}
-    state = State(**dic)
-    storage.new(state)
-    dic = {"name": "Mexico", "state_id": state.id}
-    city = City(**dic)
-    storage.new(city)
-    storage.save()
-    c = storage.count()
-    self.assertEqual(len(storage.all()), c)
+        """ Tests count method db storage """
+        dic = {"name": "Vecindad"}
+        state = State(**dic)
+        storage.new(state)
+        dic = {"name": "Mexico", "state_id": state.id}
+        city = City(**dic)
+        storage.new(city)
+        storage.save()
+        c = storage.count()
+        self.assertEqual(len(storage.all()), c)

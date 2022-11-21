771f0b93e26adab66b963c2f02485e89b94cf7e6
TFrankie55 <towoeye50@gmail.com>
2022-11-21 14:18:45 -0800

    returning the count of all class obj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,30 @@
 """ Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Method to return app status """
     return jsonify(status='OK')
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Method to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+            }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)

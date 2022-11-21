efb589950c499baa0d4a8d3af8f53947ec6a66fc
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-21 17:02:32 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,4 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Method to return app status """
-    return jsonify("status=OK")
+    return jsonify(status=OK)

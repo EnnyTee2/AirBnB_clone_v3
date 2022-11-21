12094608e5efb00493c6c10642fadd058da8632d
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-21 13:22:25 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
+""" Flask Application """
 
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(error):
     """ Method to teardown app context """
-    return storage.close()
+    storage.close()
 
 
 if __name__ == "__main__":
-    if getenv('HBNB_API-HOST') != none:
-        hostt = getenv('HBNB_API-HOST')
+    if getenv('HBNB_API_HOST') != none:
+        hostt = getenv('HBNB_API_HOST')
     else:
-        hostt = "0.0.0.0"
+        hostt = '0.0.0.0'
     if getenv('HBNB_API_PORT') != none:
-        portt = int(getenv('HBNB_API_PORT'))
+        portt = getenv('HBNB_API_PORT'))
     else:
-        portt = 5000
+        portt = '5000'
 
     app.run(host=hostt, port=portt, threaded=True)

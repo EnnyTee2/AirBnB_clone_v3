f207fe2f1bba870e85210f59f8b4ddd6633a0336
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-21 23:25:34 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+""" Index """
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def all_states():
+    """ Method to retrieve list of all states """
+    all_states = storage.all('State')
+    return jsonify(all_states)

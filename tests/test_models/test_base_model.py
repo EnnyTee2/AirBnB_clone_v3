f645c12b52cbe340377ee1f40a7ef7c9cfff5f19
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-21 21:57:01 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,7 +7,7 @@ import pep8 as pycodestyle
 import time
 import unittest
 from unittest import mock
-BaseModel = models.base_model.BaseModel
+from models.base_model import BaseModel
 module_doc = models.base_model.__doc__
 
 

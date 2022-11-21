62f288b10d5951c72777bf9e9049cfaef26e8c07
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-21 23:37:39 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,5 +8,8 @@ from models import storage
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def all_states():
     """ Method to retrieve list of all states """
-    all_states = storage.get('State')
+    all_states = storage.all('State').values()
+    state_list = []
+    for state in all_states:
+        state_list.append(state.to_dict())
     return jsonify(all_states)

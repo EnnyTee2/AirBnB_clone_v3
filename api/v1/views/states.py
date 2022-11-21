d75e7534449453207235b331b79e362c2da81f26
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-21 23:46:06 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,3 +13,9 @@ def all_states():
     for state in all_states:
         state_list.append(state.to_dict())
     return jsonify(state_list)
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def all_states(state_id):
+    """ Method to retrieve list of a specific state by id """
+    statey = storage.get('State', state_id)
+    return jsonify(statey)

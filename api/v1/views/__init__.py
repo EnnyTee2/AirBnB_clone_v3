01bfda3436072c320ce1c46e313b55fc5e09f4ad
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-22 00:43:11 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,39 +1,9 @@
 #!/usr/bin/python3
-""" Flask Application that runs based on an imported Blueprint """
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
-from os import getenv
+""" Blueprint for API """
+from flask import Blueprint
+from api.v1.views.states import *
+from api.v1.views.index import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
 
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def teardown(error):
-    """ Method to teardown app context """
-    storage.close()
-
-
-""" Main Application """
-if __name__ == "__main__":
-    hostt = getenv('HBNB_API_HOST')
-    portt = getenv('HBNB_API_PORT')
-
-    hostt = '0.0.0.0' if not hostt else HBNB_API_HOST
-    portt = 5000 if not portt else HBNB_API_PORT
-    app.run(host=hostt, port=portt, threaded=True)
-    
-    
-    @app.errorhandler(404)
-    def handle_404(exception):
-    """
-    handles 404 errors, in the event that global error handler fails
-    """
-    code = exception.__str__().split()[0]
-    description = exception.description
-    message = {'error': description}
-    return make_response(jsonify(message), code)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
